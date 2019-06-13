2cf2209823d4e04ea7be80e088b558adbc530deb
resault <resault.m@gmail.com>
2019-06-13 17:48:37 +0900

    6/13

--- MovieHolic/src/com/kitri/util/SiteConstance.java
+++ MovieHolic/src/com/kitri/util/SiteConstance.java
@@ -9,8 +9,8 @@ public class SiteConstance {
 	public static final String DB_DRIVER = "oracle.jdbc.driver.OracleDriver";
 	public static final String DB_URL = "jdbc:oracle:thin:@localhost:1521:orcl";
 
-	public static final String DB_USERNAME = "kitiri";
-	public static final String DB_PASSWORD = "kitri";
+	public static final String DB_USERNAME = "MovieHolic";
+	public static final String DB_PASSWORD = "MovieHolic";
 
 	
 }
