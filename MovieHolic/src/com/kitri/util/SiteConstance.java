70101d3f4abe384676863077d127976178424d60
shzy232 <shzy232@naver.com>
2019-06-14 10:49:16 +0900

    06.14 SJY

--- MovieHolic/src/com/kitri/util/SiteConstance.java
+++ MovieHolic/src/com/kitri/util/SiteConstance.java
@@ -9,7 +9,7 @@ public class SiteConstance {
 	public static final String DB_DRIVER = "oracle.jdbc.driver.OracleDriver";
 	public static final String DB_URL = "jdbc:oracle:thin:@localhost:1521:orcl";
 
-	public static final String DB_USERNAME = "kitiri";
+	public static final String DB_USERNAME = "kitri";
 	public static final String DB_PASSWORD = "kitri";
 
 	
